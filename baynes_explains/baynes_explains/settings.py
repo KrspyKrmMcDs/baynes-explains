be06067d40abda214212158d56ecd1f4f3a2578b
JacobBaynes <jacob.baynes@icloud.com>
2022-10-03 13:28:48 -0400

    I changed the static and templates structure to be at project level instead of app level.

--- baynes_explains/baynes_explains/settings.py
+++ baynes_explains/baynes_explains/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'baynes_explains.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/blog'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
